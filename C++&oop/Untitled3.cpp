b9c233393f5961095ad8f1b6a278d9c31f298669
alinecarine <138150662+alinecarine@users.noreply.github.com>
2025-02-20 19:10:29 +0200

    Add files via upload

---
+++ C++&oop/Untitled3.cpp
@@ -0,0 +1,13 @@
+//this is for doing differents opperations like addtion,multiplications,subtraction ,etc.
+#include <iostream>
+using namespace std;
+int main(){
+
+int a = 10, b = 3;
+cout << "Sum: " << a + b << endl;
+cout << "Difference: " << a - b << endl;
+cout << "Product: " << a * b << endl;
+cout << "Quotient: " << a / b << endl;
+cout << "Remainder: " << a % b << endl;
+return 0;
+}
---
+++ C++&oop/hello.cpp
@@ -0,0 +1,10 @@
+// simple code to display hello word
+#include <iostream>  //
+ using namespace std; 
+
+int main(){//this is main function
+    cout <<"Hello, World!";// and this is out put to be displayed
+    return 0;   
+}
+
+
